22d33adfc211d62a4e7066e32255f734918a338c
bleeeeek <mame0009@student.monash.edu>
2024-12-20 11:41:25 +0800

    updated perms

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/VOG/' // Ensure this matches your repository name
+  base: '/VOG/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 });
